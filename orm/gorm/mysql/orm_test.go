3f3b5f113064e3f283cbbde90161f27f1ee7592d
yebaozhi <yebaozhi@huojintech.com>
2019-10-30 15:59:30 +0800

    fix time ptr, add creat func

--- orm/gorm/mysql/orm_test.go
+++ orm/gorm/mysql/orm_test.go
@@ -1,14 +1,16 @@
 package mysql
 
 import (
-	db_config "github.com/dipperin/go-ms-toolkit/db-config"
+	"testing"
+
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/stretchr/testify/assert"
-	"testing"
+
+	dbConfig "github.com/dipperin/go-ms-toolkit/db-config"
 )
 
 func Test_newGormMysql(t *testing.T) {
-	conf := db_config.NewDbConfig()
+	conf := dbConfig.NewDbConfig()
 	conf.DbName = "hahaha_test"
 
 	gm := newGormMysql(conf, true)
@@ -20,5 +22,27 @@ func Test_newGormMysql(t *testing.T) {
 	gm2 := newGormMysql(conf, false)
 	assert.NotNil(t, gm2)
 	assert.NotNil(t, gm2.GetDB())
+	gm2.ClearAllData()
+}
+
+func Test_gormMysql_Create(t *testing.T) {
+	conf := dbConfig.NewDbConfig()
+	conf.DbName = "hahaha_test"
+
+	gm := newGormMysql(conf, true)
+	assert.NotNil(t, gm)
+	gm.CreateDB()
+	gm.GetUtilDB()
+	defer gm.DropDB()
+
+	type Test struct {
+		Data string `gorm:"data"`
+	}
+	gm2 := newGormMysql(conf, false)
+	gm2.GetDB().AutoMigrate(&Test{})
+	assert.NotNil(t, gm2)
+	assert.NoError(t, gm2.Create(&Test{}))
+	assert.NoError(t, gm2.Create([]*Test{}))
+	assert.NoError(t, gm2.Create([]*Test{{Data: "1"}, {Data: "2"}, {Data: "3"}}))
 	gm2.ClearAllData()
 }
